e3033b97b71d5d866fd7df26d08951c64eb61fcb
robertsedhom <robertsidhom@gmail.com>
2017-10-11 12:13:18 +0200

    init commit

---
+++ lib/paytm.ex
@@ -0,0 +1,18 @@
+defmodule Paytm do
+  @moduledoc """
+  Documentation for Paytm.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Paytm.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/paytm_test.exs
@@ -0,0 +1,8 @@
+defmodule PaytmTest do
+  use ExUnit.Case
+  doctest Paytm
+
+  test "greets the world" do
+    assert Paytm.hello() == :world
+  end
+end
