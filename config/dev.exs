ecaa8f9f20c0c2728d07b7cb820df99ddf26ab35
Nihal Gonsalves <nihal@nihalgonsalves.com>
2017-10-24 19:01:15 +0530

    Add config for Wallet API (merchant key/id & balance URL)

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
 config :paytm, Paytm.API.Wallet,
-  base_url: "https://pguat.paytm.com"
+  base_url:          "https://pguat.paytm.com",
+  check_balance_url: "https://trust-uat.paytm.in/service/checkUserBalance"
 
 config :paytm, Paytm.API.OAuth,
   base_url: "https://accounts-uat.paytm.com"
