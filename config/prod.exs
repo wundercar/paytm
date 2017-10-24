d3af969d0d47277b98f945c8dd3a811aa13e981a
Nihal Gonsalves <nihal@nihalgonsalves.com>
2017-10-24 11:35:38 +0530

    Add OAuth config variables

--- config/prod.exs
+++ config/prod.exs
@@ -3,3 +3,7 @@ use Mix.Config
 config :paytm, Paytm,
   oauth_api_base_url: "https://accounts.paytm.com",
   wallet_api_base_url: "https://secure.paytm.in"
+
+config :paytm, Paytm.API.OAuth,
+  client_id: "",
+  client_secret: ""
