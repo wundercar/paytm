883c87d109ef8596a630eac5b101b3424c0234ac
Nihal Gonsalves <nihal@nihalgonsalves.com>
2017-10-24 13:10:32 +0530

    Add ExVCR

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Paytm.Mixfile do
     [
       {:httpoison, "~> 0.13"},
       {:poison, "~> 3.1"},
+      {:exvcr, "~> 0.8", only: :test},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
