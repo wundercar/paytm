4c51772661489003bad8f624e31e7b5872fe1a88
Nihal Gonsalves <nihal@nihalgonsalves.com>
2017-10-26 12:02:49 +0530

    Add  config

--- config/config.exs
+++ config/config.exs
@@ -1,15 +1,16 @@
 use Mix.Config
 
 config :paytm, Paytm.API.Wallet,
-  base_url: "https://pguat.paytm.com",
+  base_url:          "https://pguat.paytm.com",
   check_balance_url: "https://trust-uat.paytm.in/service/checkUserBalance",
-  merchant_id: System.get_env("PAYTM_MERCHANT_ID"),
-  merchant_key: System.get_env("PAYTM_MERCHANT_KEY"),
-  callback_url: System.get_env("PAYTM_CALLBACK_URL")
+  merchant_id:       System.get_env("PAYTM_MERCHANT_ID"),
+  merchant_key:      System.get_env("PAYTM_MERCHANT_KEY"),
+  merchant_website:  System.get_env("PAYTM_MERCHANT_WEBSITE"),
+  callback_url:      System.get_env("PAYTM_CALLBACK_URL")
 
 config :paytm, Paytm.API.OAuth,
-  base_url: "https://accounts-uat.paytm.com",
-  client_id: System.get_env("PAYTM_OAUTH_CLIENT_ID"),
-  client_secret: System.get_env("PAYTM_OAUTH_CLIENT_SECRET")
+  base_url:          "https://accounts-uat.paytm.com",
+  client_id:         System.get_env("PAYTM_OAUTH_CLIENT_ID"),
+  client_secret:     System.get_env("PAYTM_OAUTH_CLIENT_SECRET")
 
 import_config "#{Mix.env}.exs"
