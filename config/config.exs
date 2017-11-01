386d46ef09f7fdc71ea118b0ff2354f094c7e9f3
Nihal Gonsalves <nihal@nihalgonsalves.com>
2017-11-01 16:50:47 +0530

    Define merchant default industry/channel config for the Wallet API

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,9 @@ config :paytm, Paytm.API.Wallet,
   merchant_id:       System.get_env("PAYTM_MERCHANT_ID"),
   merchant_key:      System.get_env("PAYTM_MERCHANT_KEY"),
   merchant_website:  System.get_env("PAYTM_MERCHANT_WEBSITE"),
-  callback_url:      System.get_env("PAYTM_CALLBACK_URL")
+  merchant_industry: System.get_env("PAYTM_MERCHANT_INDUSTRY"),
+  callback_url:      System.get_env("PAYTM_CALLBACK_URL"),
+  channel_id:        System.get_env("PAYTM_DEFAULT_CHANNEL")
 
 config :paytm, Paytm.API.OAuth,
   base_url:          "https://accounts-uat.paytm.com",
